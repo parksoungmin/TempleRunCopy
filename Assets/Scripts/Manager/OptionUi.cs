cccf1754455aa886a24b10de906e0e219720b3c7
parksoungmin <psmin5812@gmail.com>
2025-02-13 14:42:18 +0900

    옵션 Ui 원래 사운드량과 Slider 값 다른 버그 수정

--- Assets/Scripts/Manager/OptionUi.cs
+++ Assets/Scripts/Manager/OptionUi.cs
@@ -6,9 +6,13 @@ using UnityEngine.UI;
 public class OptionUi : MonoBehaviour
 {
     public SoundManager SoundManager;
+
+    public Slider sfxSlider;
+    public Slider bgmSlider;
     void Start()
     {
         SoundManager.SoundOptionSet();
+        sfxSlider.value = SaveLoadManager.Data.sfxSound;
+        bgmSlider.value = SaveLoadManager.Data.bgmSound;
     }
-
 }
